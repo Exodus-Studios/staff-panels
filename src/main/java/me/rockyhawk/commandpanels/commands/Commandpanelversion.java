c161fda98eaf115aae2ff74919cf5a9a1d35e474
reussy <imreussy@gmail.com>
2023-02-27 21:11:40 -0600

    Removido Updater.java

--- src/main/java/me/rockyhawk/commandpanels/commands/Commandpanelversion.java
+++ src/main/java/me/rockyhawk/commandpanels/commands/Commandpanelversion.java
@@ -16,32 +16,16 @@ public class Commandpanelversion implements CommandExecutor {
         if (label.equalsIgnoreCase("spv") || label.equalsIgnoreCase("staffpanelversion") || label.equalsIgnoreCase("spanelv")) {
             if(args.length == 0) {
                 if (sender.hasPermission("staffpanel.version")) {
-                    //version command
-                    String latestVersion = plugin.updater.getLatestVersion(false);
                     sender.sendMessage(plugin.tex.colour(plugin.tag));
                     sender.sendMessage(ChatColor.GREEN + "This Version   " + ChatColor.GRAY + plugin.getDescription().getVersion());
-                    sender.sendMessage(ChatColor.GREEN + "Latest Version " + ChatColor.GRAY + latestVersion);
                     sender.sendMessage(ChatColor.GRAY + "-------------------");
                     sender.sendMessage(ChatColor.GREEN + "Developer " + ChatColor.GRAY + "RockyHawk");
                     sender.sendMessage(ChatColor.GREEN + "Command   " + ChatColor.GRAY + "/sp");
                 } else {
                     sender.sendMessage(plugin.tex.colour(plugin.tag + plugin.config.getString("config.format.perms")));
                 }
-            }else if(args.length == 1){
-                if (sender.hasPermission("staffpanel.update")) {
-                    if (args[0].equals("cancel")) {
-                        plugin.updater.downloadVersionManually = null;
-                        sender.sendMessage(plugin.tex.colour(plugin.tag + ChatColor.GREEN + "Will not download a new version on restart."));
-                    } else {
-                        plugin.updater.downloadVersionManually = args[0];
-                        sender.sendMessage(plugin.tex.colour(plugin.tag + ChatColor.GREEN + "Downloading version " + ChatColor.GRAY + args[0] + ChatColor.GREEN + " upon server restart."));
-                    }
-                }else{
-                    sender.sendMessage(plugin.tex.colour(plugin.tag + plugin.config.getString("config.format.perms")));
-                }
-            }else{
-                sender.sendMessage(plugin.tex.colour(plugin.tag + ChatColor.RED + "Usage: /spv [update:latest:cancel]"));
             }
+
             return true;
         }
         return true;
